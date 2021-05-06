ce14131e894bfe28ac2877beaa6c28e092fad554
Nikolay Kiselev <55307887+kiselev-nikolay@users.noreply.github.com>
2021-05-06 23:03:27 +0300

    Gin version of ha http proxy

--- gin/go.mod
+++ gin/go.mod
@@ -1,3 +1,8 @@
 module github.com/kiselev-nikolay/ha-http-proxy/gin
 
 go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/kiselev-nikolay/ha-http-proxy/light v0.0.0-20210506193932-2eabddd8e1d6
+)
--- gin/main.go
+++ gin/main.go
@@ -1,1 +1,70 @@
 package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kiselev-nikolay/ha-http-proxy/light/pkg/trace"
+)
+
+type request struct {
+	Method  string            `json:"method" binding:"required"`
+	RawURL  string            `json:"url" binding:"required"`
+	Headers map[string]string `json:"headers" binding:"required"`
+}
+
+type response struct {
+	ID      string            `json:"id"`
+	Status  int               `json:"status"`
+	Length  int               `json:"length"`
+	Headers map[string]string `json:"headers"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/proxy", func(g *gin.Context) {
+		req := &request{}
+		err := g.BindJSON(req)
+		if err != nil {
+			g.JSON(http.StatusBadRequest, gin.H{"errors": []string{err.Error()}})
+			return
+		}
+		url, err := url.Parse(req.RawURL)
+		if err != nil {
+			g.JSON(http.StatusBadRequest, gin.H{"errors": []string{err.Error()}})
+		}
+		reqHeaders := make(http.Header)
+		for k, v := range req.Headers {
+			reqHeaders.Add(k, v)
+		}
+		traceID := trace.GetID()
+		reqHeaders.Add("X-Hhp-Trace-Id", traceID)
+		res, err := http.DefaultClient.Do(&http.Request{
+			Method: req.Method,
+			URL:    url,
+			Header: reqHeaders,
+		})
+		if err != nil {
+			g.JSON(http.StatusBadGateway, gin.H{"errors": []string{err.Error()}})
+			return
+		}
+		resHeaders := make(map[string]string)
+		for k, v := range res.Header {
+			resHeaders[k] = v[0]
+		}
+		length := 0
+		responseBody, err := ioutil.ReadAll(res.Body)
+		if err == nil {
+			length = len(responseBody)
+		}
+		g.JSON(200, response{
+			ID:      traceID,
+			Status:  res.StatusCode,
+			Headers: resHeaders,
+			Length:  length,
+		})
+	})
+	r.Run()
+}
